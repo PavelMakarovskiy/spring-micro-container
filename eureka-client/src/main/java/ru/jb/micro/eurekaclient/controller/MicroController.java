5ad3a509da3a1102e0c038c44e2f4715410eb3a7
pmakarovskiy <pavel.makarovskiy@paygine.net>
2023-11-23 17:10:18 +0300

    added api-gateway

--- eureka-client/src/main/java/ru/jb/micro/eurekaclient/controller/MicroController.java
+++ eureka-client/src/main/java/ru/jb/micro/eurekaclient/controller/MicroController.java
@@ -1,13 +1,15 @@
 package ru.jb.micro.eurekaclient.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/main")
 public class MicroController {
 
     @GetMapping("/micro")
     public String micro() {
-        return "micro";
+        return "micro route";
     }
 }
