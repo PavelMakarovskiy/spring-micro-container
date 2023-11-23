781b9b95bf63fb49c9e346bd0d70a959ebf5a42c
pmakarovskiy <pavel.makarovskiy@paygine.net>
2023-11-23 11:47:15 +0300

    start micro pet project

---
+++ eureka-server/src/main/java/ru/jb/micro/eurekaserver/EurekaServerApplication.java
@@ -0,0 +1,13 @@
+package ru.jb.micro.eurekaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EurekaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaServerApplication.class, args);
+	}
+
+}
