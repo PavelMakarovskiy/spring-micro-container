1b6d2ef77e07ecb54093645008cd6ac1e383bbee
pmakarovskiy <pavel.makarovskiy@paygine.net>
2023-11-30 19:33:04 +0300

    added standard ports

--- eureka-client/src/main/java/ru/jb/micro/eurekaclient/controller/MicroController.java
+++ eureka-client/src/main/java/ru/jb/micro/eurekaclient/controller/MicroController.java
@@ -1,5 +1,6 @@
 package ru.jb.micro.eurekaclient.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,8 +9,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/main")
 public class MicroController {
 
+    @Value("${eureka.instance.instance-id}")
+    private String id;
+
     @GetMapping("/micro")
     public String micro() {
-        return "micro route";
+        return id;
     }
 }
